86252fec278fd116f82be63e6c4966d1bd5a9fbb
Leigh Zhu <i@zhuli.me>
2013-02-15 02:30:26 +0800

    init

---
+++ world-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# --config sample
+# --use backword slash if your world name has space, ep: Hello/ World
+
+# WordName=SampleWorld
+# MCPATH=/home/$(whoami)/.minecraft
+# BKDIR=/home/$(whoami)/MC
+# DROPBOX=1
+
+#-- sample end
+
+
+timestamp=`date  +%Y-%m-%d_%H%M%S`
+cd ~/
+tar czvf $(BKDIR)/$(WordName)-${timestamp}-shell.tar.gz $(MCPATH)/saves/$(WordName)
+echo $(md5sum $(BKDIR)/$(WordName)-${timestamp}-shell.tar.gz)>>$(BKDIR)/hash.log
+
+if [ $DROPBOX -eq 1 ]; then
+	cp $(BKDIR)/$(WordName)-${timestamp}-shell.tar.gz /home/$(whoami)/Dropbox/MC/ 
+fi
